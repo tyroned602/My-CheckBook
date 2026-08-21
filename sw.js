d9148f29bad5594c035fe68869ac0a171a6a3757
tyroned602 <tyroned602@gmail.com>
2026-08-20 20:50:19 -0700

    Refresh PWA cache for account controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'checkbook-v1';
+const CACHE = 'checkbook-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
